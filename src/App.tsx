aa2288807fc0a039de0a92679281a3bd5ced1b18
ddb0240 <135185056+ddb0240@users.noreply.github.com>
2024-11-24 23:24:45 -0600

    Added name

--- src/App.tsx
+++ src/App.tsx
@@ -50,7 +50,7 @@ function App() {
         <h1 className="main-header">
           Meet Your Personal
           <br />
-          <span className="highlight">Recipe AI</span>
+          <span className="highlight">Recipe AI by Dan Bui</span>
         </h1>
         <p className="description">
           Simply type a few ingredients using the format ingredient1,
